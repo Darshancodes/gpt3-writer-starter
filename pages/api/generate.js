668a662b981e37ee1fd405ea7d9aaef3d03a4a31
Darshancodes <70362500+Darshancodes@users.noreply.github.com>
2023-01-14 09:49:43 +0000

    add: new api key

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,19 +5,44 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = "";
+
+const basePromptPrefix =
+`
+Write me a project ideas with the title below. Please make sure the project ideas goes in-depth on the topic and shows that this is unique ideas.
+Title:
+`
+
 const generateAction = async (req, res) => {
-  // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
+    temperature: 0.8,
     max_tokens: 250,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
+
+  const secondPrompt = 
+  `
+  Take the project ideas, and list how you can build them with the given title.
+  
+  `
+  
+
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+
+    temperature: 0.85,
+
+    max_tokens: 1250,
+  });
+  
+
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
 
   res.status(200).json({ output: basePromptOutput });
 };
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,64 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { useState } from 'react';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
-  const [apiOutput, setApiOutput] = useState('')
-  const [isGenerating, setIsGenerating] = useState(false)
-
-const callGenerateEndpoint = async () => {
-  setIsGenerating(true);
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
   
-  console.log("Calling OpenAI...")
-  const response = await fetch('/api/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userInput }),
-  });
-
-  const data = await response.json();
-  const { output } = data;
-  console.log("OpenAI replied...", output.text)
-
-  setApiOutput(`${output.text}`);
-  setIsGenerating(false);
-}
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
   };
-
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT3 Project Ideas Generator</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
             <h1>Project ideas generator</h1>
           </div>
+          <div className='header-title-1'>
+            <h2>Built with GPT-3</h2>
+          </div>
           <div className="header-subtitle">
-            <h2>Generate awesome project ideas in seconds!</h2>
+            <h2>Confused in which project to make? Give me a prompt and I got your back</h2>
           </div>
         </div>
         <div className="prompt-container">
-  <textarea
-    placeholder="start typing here"
-    className="prompt-box"
-    value={userInput}
-    onChange={onUserChangedText}
-  />
-  {/* New code I added here */}
-  <div className="prompt-buttons">
-    <a className="generate-button" onClick={callGenerateEndpoint}>
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText}/>
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
       <div className="generate">
-        <p>Generate</p>
+      {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
       </div>
     </a>
   </div>
-
-{/* New code I added here */}
-{apiOutput && (
+  {apiOutput && (
   <div className="output">
     <div className="output-header-container">
       <div className="output-header">
@@ -75,22 +70,21 @@ const callGenerateEndpoint = async () => {
     </div>
   </div>
 )}
-
-</div>
+        </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://twitter.com/Darshanshub"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built by Darshancodes</p>
           </div>
         </a>
       </div>
-    </div >
+    </div>
   );
 };
 
